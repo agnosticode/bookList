eae98083e4f656a2bfc373f1c6cd5c63ffc66126
Satinder Singh <agnosticode@gmail.com>
2018-04-03 02:44:59 +0530

    Add Local Storage

--- appes6.js
+++ appes6.js
@@ -54,6 +54,51 @@ class UI {
     }
 };
 
+//Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }else{
+            books=JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static displayBooks(){
+        const books=Store.getBooks();
+        books.forEach(function(book){
+
+            //Instantiating UI Class
+            const ui=new UI();
+
+            //Add Book To UI
+            ui.addBookToList(book); 
+        });
+    }
+
+    static addBook(book){
+        const books=Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+
+    static removeBook(isbn){
+        const books=Store.getBooks();
+        books.forEach(function(book,index){
+            if(book.isbn===isbn){
+                books.splice(index,1);
+            }
+        });
+
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+}
+
+//DOM Load Event
+document.addEventListener('DOMContentLoaded',Store.displayBooks);
+
 //Event Listeners for add book
 document.querySelector('#book-form').addEventListener('submit', function (e) {
     //Get form values
@@ -74,6 +119,9 @@ document.querySelector('#book-form').addEventListener('submit', function (e) {
     } else {
         //Add book to list
         ui.addBookToList(book);
+
+        //Add To Local Storage
+        Store.addBook(book);
 
         //Success Alert
         ui.showAlert('Book Added', 'success');
@@ -96,6 +144,12 @@ document.querySelector('#book-list').addEventListener('click', function (e) {
     //Delete Book
     ui.deleteBook(e.target);
 
+    //Remove From Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+
     //Show Message
-    ui.showAlert('Book Removed', 'success');
+    if (e.target.className === 'delete') {
+        ui.showAlert('Book Removed', 'success');
+    };
+    
 });
